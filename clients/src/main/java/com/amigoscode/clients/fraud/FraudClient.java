bd23254b46bafab6a30899c45070a950537d173a
youssefbenihoud <youssefbenihoud+github@gmail.com>
2022-10-11 18:25:02 +0200

    3 - Using Feign Client

--- clients/src/main/java/com/amigoscode/clients/fraud/FraudClient.java
+++ clients/src/main/java/com/amigoscode/clients/fraud/FraudClient.java
@@ -2,6 +2,8 @@ package com.amigoscode.clients.fraud;
 
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
         value = "fraud",
@@ -13,5 +15,7 @@ public interface FraudClient {
 
     // @GetMapping(path="api/v1/fraud-check/api/v1/fraud-check/{customerId}")
     // FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerID);
-    // You need to take FraudCheckResponse File into this package ..clients.fraud
+
+    @GetMapping("{customerId}")
+    FraudCheckResponse isFraudster(@PathVariable Integer customerId);
 }
--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -1,5 +1,7 @@
 package com.amigoscode.customer;
 
+import com.amigoscode.clients.fraud.FraudCheckResponse;
+import com.amigoscode.clients.fraud.FraudClient;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +12,7 @@ public class CustomerService {
 
     private final CustomerRespository customerRespository;
     private final RestTemplate restTemplate;
+    private final FraudClient fraudClient;
 
     public void registerCustomer(CustomerRegistrationRequest registrationRequest){
         Customer customer = Customer.builder()
@@ -22,11 +25,8 @@ public class CustomerService {
         //todo: check if email not taken
         customerRespository.saveAndFlush(customer);
         //todo: check if fraudster
-        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://FRAUD/api/v1/fraud-check/{customerId}",
-                FraudCheckResponse.class,
-                customer.getId()
-        );
+
+        FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(customer.getId());
         if(fraudCheckResponse.isFraudster()){
             throw new IllegalStateException("Fraudster");
         }
