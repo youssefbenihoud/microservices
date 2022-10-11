b2e60260375e28c70a1acd63632fbb94f94ad54f
Youssef Benihoud <youssefbenihoud+github@gmail.com>
2022-10-11 17:54:35 +0200

    6 - Add Load Balancer in RestTemplate (#1)

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -23,7 +23,7 @@ public class CustomerService {
         customerRespository.saveAndFlush(customer);
         //todo: check if fraudster
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
