a52837eacbd1159a4c417d5cb21de894dc0deea3
youssefbenihoud <youssefbenihoud+github@gmail.com>
2022-10-11 21:46:29 +0200

    4 - Microservices Communication With Feign

--- clients/src/main/java/com/amigoscode/clients/fraud/FraudClient.java
+++ clients/src/main/java/com/amigoscode/clients/fraud/FraudClient.java
@@ -5,17 +5,17 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+// Alternative ->
+//@FeignClient("fraud")
+
 @FeignClient(
         value = "fraud",
-        path = "api/v1/fraud-check"
-)
-
-// Alternative -> @FeignCLient("fraud")
+        path = "api/v1/fraud-check")
 public interface FraudClient {
 
-    // @GetMapping(path="api/v1/fraud-check/api/v1/fraud-check/{customerId}")
-    // FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerID);
+    @GetMapping(path = "/{customerId}")
+    FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerID);
 
-    @GetMapping("{customerId}")
-    FraudCheckResponse isFraudster(@PathVariable Integer customerId);
+//    @GetMapping("{customerId}")
+//    FraudCheckResponse isFraudster(@PathVariable Integer customerId);
 }
