1da784dde175d93d55b6bf890aa9b6f60b6091df
youssefbenihoud <youssefbenihoud+github@gmail.com>
2022-10-16 15:31:48 +0200

    Add NotificationConfig

--- amgp/src/main/java/com/amigoscode/amqp/RabbitMQConfig.java
+++ amgp/src/main/java/com/amigoscode/amqp/RabbitMQConfig.java
@@ -3,6 +3,7 @@ package com.amigoscode.amqp;
 
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -26,8 +27,18 @@ public class RabbitMQConfig {
     }
 
     @Bean
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory(){
+        SimpleRabbitListenerContainerFactory factory =
+                new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jacksonConverter());
+        return factory;
+    }
+
+    @Bean
     public MessageConverter jacksonConverter() {
-        MessageConverter jackson2JsonMessageConverter = new Jackson2JsonMessageConverter();
+        MessageConverter jackson2JsonMessageConverter =
+                new Jackson2JsonMessageConverter();
         return jackson2JsonMessageConverter;
     }
 
