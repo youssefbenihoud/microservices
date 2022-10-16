cefa3a8281d4737c1b43d83d1590937efba0428d
youssefbenihoud <youssefbenihoud+github@gmail.com>
2022-10-16 13:59:01 +0200

    Add RabbitMQ Configuration

---
+++ amgp/src/main/java/com/amigoscode/amqp/RabbitMQConfig.java
@@ -0,0 +1,34 @@
+package com.amigoscode.amqp;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+@Configuration
+@AllArgsConstructor
+public class RabbitMQConfig {
+
+
+    private final ConnectionFactory connectionFactory;
+
+    @Bean
+    public AmqpTemplate amqpTemplate() {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jacksonConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public MessageConverter jacksonConverter() {
+        MessageConverter jackson2JsonMessageConverter = new Jackson2JsonMessageConverter();
+        return jackson2JsonMessageConverter;
+    }
+
+}
