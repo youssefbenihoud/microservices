c36ca22033229232cffbbc7d3983944c35b85640
youssefbenihoud <youssefbenihoud+github@gmail.com>
2022-10-17 13:42:37 +0200

    9 - RabbitListner

---
+++ notification/src/main/java/com/amigoscode/notification/rabbitmq/NotificationConsumer.java
@@ -0,0 +1,24 @@
+package com.amigoscode.notification.rabbitmq;
+
+import com.amigoscode.clients.notification.NotificationRequest;
+import com.amigoscode.notification.NotificationService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class NotificationConsumer {
+
+    private final NotificationService notificationService;
+
+    @RabbitListener(queues = "${rabbitmq.queue.notification}")
+    public void consumer(NotificationRequest notificationRequest){
+      log.info("Consumed {} from queue", notificationRequest);
+      notificationService.send(notificationRequest);
+    }
+
+}
